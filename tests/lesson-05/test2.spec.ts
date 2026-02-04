a0e6b503d1a352e07e45b563d006e6b3a47ce5d3
test01 - ThuPhan <thuphan.vm@gmail.com>
2026-02-05 00:18:53 +0700

    feat: Lesson5 - add file test2.spec.ts

---
+++ tests/lesson-05/test2.spec.ts
@@ -0,0 +1,40 @@
+/* Truy cập trang https://material.playwrightvn.com/, click vào “Bài
+học 1: Register Page (có đủ các element) */
+
+import { test, expect } from '@playwright/test';
+
+const link = 'https://material.playwrightvn.com/';
+
+test('Excercise 2: Product Page', async ({ page }) => {
+
+    await test.step('Navigate to Material Playwright Page', async () => {
+        // navigate to the link
+        await page.goto(link);
+    });
+
+    await test.step('Click on Bài học 2: Product page', async () => {
+        // click to the link
+        await page.getByRole('link', { name: 'Bài học 2: Product page' }).click();
+
+        // Expects page to have a heading with the name of User Registration.
+        await expect(page.getByRole('heading', { name: 'Simple E-commerce' })).toBeVisible(); // heading = find any tag <h1>, <h2>, ...
+    });
+
+    //Handle add products to cart
+    await test.step('Add product to cart', async () => {
+        // add product 1: 2 
+        await page.locator("//button[text()='Add to Cart' and @data-product-id = '1']").click({
+            clickCount: 2
+        });
+
+        // add product 2: 3
+        await page.locator("//button[@data-product-id = '2']").click({
+            clickCount: 3
+        });
+
+        // add product 3: 1
+        await page.locator("(//button)[3]").click({
+            clickCount: 1
+        });
+    })
+});
